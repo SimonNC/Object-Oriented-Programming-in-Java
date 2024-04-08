c80b7dbe892cf843f7aaa0cb07569b0b4c70dc7b
SimonNC <simon.jorite@gmail.com>
2024-04-08 11:08:27 +1100

    first commit

---
+++ src/ArrayLocation.java
@@ -0,0 +1,10 @@
+
+public class ArrayLocation {
+	private double coords[];
+	public ArrayLocation(double[] coords) {
+		this.coords= coords;
+	}
+	public double[] getCoords() {
+		return this.coords;
+	}
+}
---
+++ src/IntAssignmentTester.java
@@ -0,0 +1,13 @@
+import demos.SimpleLocation;
+
+public class IntAssignmentTester {
+	public static void main(String[] args) {
+	double[] coords = {5.0,0.0};
+	ArrayLocation accra = new ArrayLocation(coords);
+	coords[0] =32.9;
+	coords[1] = -117.2;
+	System.out.println(accra.getCoords()[0]);
+		
+	}
+
+}
